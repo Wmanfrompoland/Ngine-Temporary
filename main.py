876c1ee7e43f96cc819e4c6143591a9c52c6eb4a
Wmanfrompoland <149189971+Wmanfrompoland@users.noreply.github.com>
2023-10-27 22:39:06 +0200

    Added "File support"

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import pygame 
 import numpy  as np
 import renderer
+from models import cube
 
 pygame.display.set_caption("Ngine")
 screen = pygame.display.set_mode((800,600))
 scale = 100
 center = (400,300)
 angle = 0
+points,lines = cube.model()
+""""
 lines =[[1,2],[2,3],[3,4],[4,1],
         [5,6],[6,7],[7,8],[8,5], 
         [1,5],[2,6],[3,7],[4,8]]
@@ -19,6 +22,7 @@ points.append(np.matrix([-1, -1, -1]))
 points.append(np.matrix([1, -1, -1]))
 points.append(np.matrix([1, 1, -1]))
 points.append(np.matrix([-1, 1, -1]))
+"""
 rendererObj = renderer.Renderer(screen,points,scale,center,lines)
 
 clock = pygame.time.Clock()
---
+++ models/cube.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def model():
+    points = []
+    points.append(np.matrix([-1, -1, 1])) 
+    points.append(np.matrix([1, -1, 1]))  
+    points.append(np.matrix([1, -1, -1]))
+    points.append(np.matrix([-1, -1, -1])) 
+    points.append(np.matrix([0, 1, 0]))
+
+    lines = [[1,2],[2,3],[3,4],[4,1],[1,5],[2,5],[3,5],[4,5]]
+
+    return points,lines
+
+
+
